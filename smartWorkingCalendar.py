4cac36ba46d0bf2b60fb6d30dcb8cda1bfbeac64
a464068 <andreagiuseppe.diiura@enel.com>
2022-09-21 21:39:27 +0200

    Updates for style

--- smartWorkingCalendar.py
+++ smartWorkingCalendar.py
@@ -5,16 +5,24 @@ smartWorkingCalendar.py
 
 An awesome module to compute the maximum number of days spent in Mordor.
 """
+import json
+from pathlib import Path
+
 import pandas as pd
+from colorama import Back, Style
 from pandas.tseries.offsets import MonthEnd
 from workalendar.europe import Italy
 
 if __name__ == '__main__':
     cal = Italy()
-    years = (2022, 2023)
+    today = pd.Timestamp.now()
+
+    with open(Path(__file__).parent / 'config.json', 'r') as f:
+        config = json.load(f)
+    years = config['years']
 
     closed_days = list(
-        map(pd.Timestamp, ['2022-10-31', '2022-12-09'])
+        map(pd.Timestamp, config['closed_days'])
     )
 
     n_smart = 13
@@ -27,15 +35,22 @@ if __name__ == '__main__':
         months = list(range(1, 13))
         for month in months:
             start = pd.Timestamp(f'{year}-{month}-01')
-            end = start + MonthEnd(1)
-            days = pd.date_range(start=start, end=end, freq='B')
-            allowed_days = pd.to_datetime(list(
-                filter(lambda x: x not in holidays, days)
-            ))
-            s = allowed_days.size
-            print(
-                f"""\n{year}-{start.strftime('%b')}
-                {allowed_days}
-                Number of working days: {s}.
-                Days in office: {s - n_smart}."""
-            )
+            if start >= today:
+                end = start + MonthEnd(1)
+                days = pd.date_range(start=start, end=end, freq='B')
+                allowed_days = pd.to_datetime(list(
+                    filter(lambda x: x not in holidays, days)
+                ))
+                s = allowed_days.size
+
+                out = \
+                    f'\n{Style.DIM}' \
+                    f'{year}-{start.strftime("%b")}' \
+                    f'{Style.RESET_ALL}\n'  \
+                    f'{allowed_days}\n' \
+                    f'Number of working days: {s}.\n'  \
+                    f'{Back.LIGHTRED_EX + Style.DIM}' \
+                    f'Days in office: {s - n_smart}.' \
+                    f'{Style.RESET_ALL}'
+
+                print(out)
